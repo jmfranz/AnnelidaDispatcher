8bd52f1678088845b2f89f96e7ed98a1a9f3e375
Juliano Franz <jufranz@gmail.com>
2017-06-28 08:42:04 -0300

    New DB Model

---
+++ AnnelidaDispatcher/Model/Record.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+#pragma warning disable 1591
+
+namespace AnnelidaDispatcher.Model
+{
+    public class Record
+    {
+        public ObjectId Id { get; set; }
+        [BsonRepresentation(BsonType.Document)]
+        public DateTime timestamp { get; set; }
+        public List<Sensors> sensors;
+
+
+        public Record()
+        {
+            timestamp = DateTime.UtcNow;
+            sensors = new List<Sensors>();
+        }
+
+    }
+
+}
