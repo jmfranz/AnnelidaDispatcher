056f7d9b36d368efd4b304328731e7c0da53b624
Juliano Franz <jufranz@gmail.com>
2017-08-19 12:14:47 -0300

    Added new sensor class

--- AnnelidaDispatcher/Model/Sensors.cs
+++ AnnelidaDispatcher/Model/Sensors.cs
@@ -12,34 +12,118 @@ namespace AnnelidaDispatcher.Model
 {
     public class Sensors
     {
-        public double temperature { get; set; }
-        public double distance { get; set; }
-        public double depth { get; set; }
-        public double s0 { get; set; }
-        public double s1 { get; set; }
-        public double s2 { get; set; }
-        public double s3 { get; set; }
-        public double s4 { get; set; }
-        public double s5 { get; set; }
-        public double s6 { get; set; }
-        public double s7 { get; set; }
         public double s8 { get; set; }
-        public double s9 { get; set; }
-        public double s10 { get; set; }
-        public double s11 { get; set; }
-        public double s12 { get; set; }
-        public double s13 { get; set; }
-        public double s14 { get; set; }
-        public double s15 { get; set; }
-        public double s16 { get; set; }
-
-        [BsonRepresentation(BsonType.Document)]
-        public DateTime timestamp { get; set; }
+        public ObjectId Id { get; set; }
+        public BackwardSector backward;
+        public CentralSector central;
+        public ForwardSector forward;
 
         public Sensors()
         {
-            timestamp = DateTime.UtcNow;
+            backward = new BackwardSector();
+            central = new CentralSector();
+            forward = new ForwardSector();
+        }
+
+        public class Enclosure1
+        {
+            public double traction;
+            public double temperature;
+            public double pressure;
+            public List<double> orientation;
+
+            public Enclosure1()
+            {
+                orientation = new List<double>();
+            }
+        }
+        public class Enclosure2
+        {
+            public double temperature;
+            public double pressure;
+            public double inputCurrent;
+            public double outputCurrent;
+            public double inputVoltage;
+            public double outputVoltage;
+        }
+        public class Enclosure3n7
+        {
+            public List<double> engineSpeed;
+            public List<double> appliedPower;
+
+            public Enclosure3n7()
+            {
+                engineSpeed = new List<double>();
+                appliedPower = new List<double>();
+            }
+        }
+        public class Enclosure4n6
+        {
+            public double temperature;
+            public double pressure;
+            public double engineControllerTemperature;
+            public double engineControllerCurrent;
+        }
+        public class Enclosure5
+        {
+            public double temperature;
+            public double pressure;
+            public List<double> orientation;
+            public double travel;
+
+            public Enclosure5()
+            {
+                orientation = new List<double>();
+            }
+        }
+        public class Enclosure8
+        {
+            public double externalTemperature;
+            public double externalPressure;
+            public bool obstructionSensor;
+        }
+
+        public class BackwardSector
+        {
+            public Enclosure1 enclosure1;
+            public Enclosure2 enclosure2;
+            public Enclosure3n7 enclosure3;
+            
+            public BackwardSector()
+            {
+                enclosure1 = new Enclosure1();
+                enclosure2 = new Enclosure2();
+                enclosure3 = new Enclosure3n7();
+            }
+        }
+
+        public class CentralSector
+        {
+            public Enclosure4n6 enclosure4;
+            public Enclosure5 enclosure5;
+            public Enclosure4n6 enclosure6;
+
+            public CentralSector()
+            {
+                enclosure4 = new Enclosure4n6();
+                enclosure5 = new Enclosure5();
+                enclosure6 = new Enclosure4n6();
+            }
+        }
+
+        public class ForwardSector
+        {
+            public Enclosure3n7 enclosure7;
+            public Enclosure8 enclosure8;
+
+            public ForwardSector()
+            {
+                enclosure7 = new Enclosure3n7();
+                enclosure8 = new Enclosure8();
+            }
         }
     }
-       
+    
 }
+
+
