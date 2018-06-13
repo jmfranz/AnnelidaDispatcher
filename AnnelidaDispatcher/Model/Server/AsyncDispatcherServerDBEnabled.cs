4c0335e29871396ddb2c0ed59dc6179231287958
Juliano Franz <jufranz@gmail.com>
2018-06-13 11:56:41 -0300

    View Model Refactoring

--- AnnelidaDispatcher/Model/Server/AsyncDispatcherServerDBEnabled.cs
+++ AnnelidaDispatcher/Model/Server/AsyncDispatcherServerDBEnabled.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using MongoDB.Bson;
+using MongoDB.Driver;
 using MongoDB.Driver.Core.WireProtocol.Messages.Encoders.BinaryEncoders;
 
 namespace AnnelidaDispatcher.Model.Server
@@ -8,10 +9,13 @@ namespace AnnelidaDispatcher.Model.Server
     public class AsyncDispatcherServerDBEnabled : AsyncDispatcherServer
     {
 
+
         /// <inheritdoc />
-        public AsyncDispatcherServerDBEnabled(MongoWrapper sensorDb, MongoWrapper controlDb, string missionName, int tcpPort) : base(tcpPort)
+        public AsyncDispatcherServerDBEnabled(string missionName, int tcpPort) : base(tcpPort)
         {
-           messageDispatchStrategy = new MongoDispatchStrategy(sensorDb,controlDb,missionName,tcpPort);
+            var sensorDb = new MongoWrapper("sensors");
+            var controlDb = new MongoWrapper("control");
+            messageDispatchStrategy = new MongoDispatchStrategy(sensorDb,controlDb,missionName,tcpPort);
         }
 
 
